1b9346f716c46ad4485496917018575b5c0b0433
Mick Isasi <isasimick@gmail.com>
2026-02-21 02:53:05 -0700

    (feat) created login page

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,5 +1,50 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
 export default function Login() {
-    return (
-        <div>Hello</div>
-    )
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+      <div className="flex flex-col gap-6 w-sm mx-auto my-auto">
+        <form>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </Field>
+            <Field>
+              <div className="flex items-center">
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </Field>
+            <Field>
+              <Button type="submit">Login</Button>
+              <FieldDescription className="text-center">
+                Don&apos;t have an account? <a href="#">Sign up</a>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </form>
+      </div>
+    </div>
+  );
 }
